6c70e71941b30c30705b4241041b1de9b86c6f15
EduardoV-V <valenteeduardo39@gmail.com>
2025-08-27 00:27:26 -0300

    atualzinado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { FaPencilAlt, FaCheck } from 'react-icons/fa';
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { db } from "./firebase";
 import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
 import CollectionForm from "./form";
